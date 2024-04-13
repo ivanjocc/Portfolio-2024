8b42c4bb71e8b423c5cccffd311b984d06bdc385
ivan <ivanjose_04@hotmail.com>
2024-04-13 18:01:51 -0400

    banner 1

--- js/language.js
+++ js/language.js
@@ -14,10 +14,10 @@ document.getElementById('lang-toggle').addEventListener('click', function (event
 function changeLanguage(lang) {
 
 	if (lang === "EN") {
-		document.querySelector('h1').textContent = "Welcome";
+		document.querySelector('h1').textContent = "Hi I'm Ivan";
 		document.querySelector('p').textContent = "This is my portfolio.";
 	} else {
-		document.querySelector('h1').textContent = "Bienvenue";
+		document.querySelector('h1').textContent = "Hi I'm Ivan";
 		document.querySelector('p').textContent = "Ceci est mon portfolio.";
 	}
 }
