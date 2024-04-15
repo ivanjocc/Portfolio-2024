561fae9323eb05af96fe708f209cf0c3bfca11f1
ivan <ivanjose_04@hotmail.com>
2024-04-14 21:55:09 -0400

    fix custom cursor mobile

--- js/customCursor.js
+++ js/customCursor.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const banner = document.getElementById('home');
-    const customCursor = document.getElementById('custom-cursor');
-    
-    banner.addEventListener('mousemove', function(e) {
-        const x = e.pageX - banner.offsetLeft - 15;
-        const y = e.pageY - banner.offsetTop - 15;
-        customCursor.style.display = 'block';
-        customCursor.style.transform = `translate3d(${x}px, ${y}px, 0)`;
-    });
+    if (window.innerWidth > 768) {
+        const banner = document.getElementById('home');
+        const customCursor = document.getElementById('custom-cursor');
+        
+        banner.addEventListener('mousemove', function(e) {
+            const x = e.pageX - banner.offsetLeft - 15;
+            const y = e.pageY - banner.offsetTop - 15;
+            customCursor.style.display = 'block';
+            customCursor.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+        });
 
-    banner.addEventListener('mouseleave', function() {
-        customCursor.style.display = 'none';
-    });
+        banner.addEventListener('mouseleave', function() {
+            customCursor.style.display = 'none';
+        });
+    } else {
+        const customCursor = document.getElementById('custom-cursor');
+        if (customCursor) {
+            customCursor.style.display = 'none';
+        }
+    }
 });
