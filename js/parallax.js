de513c7647eb5fd66c60506fc21a238e449bb729
ivan <ivanjose_04@hotmail.com>
2024-04-14 21:14:25 -0400

    media queries projects

--- js/parallax.js
+++ js/parallax.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            } else {
-                entry.target.classList.remove('visible');
-            }
+    if (window.innerWidth > 768) {
+        let observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                } else {
+                    entry.target.classList.remove('visible');
+                }
+            });
+        }, {
+            threshold: 0.3
         });
-    }, {
-        threshold: 0.5
-    });
 
-    const sections = document.querySelectorAll('section:not(#home)');
-    sections.forEach(section => {
-        section.classList.add('fade-in-section');
-        observer.observe(section);
-    });
+        const sections = document.querySelectorAll('section:not(#home)');
+        sections.forEach(section => {
+            section.classList.add('fade-in-section');
+            observer.observe(section);
+        });
+    }
 });
